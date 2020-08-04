e1a52528dccb5100fdc9c07ef0de48a1e7fd2117
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-04 11:51:33 -0400

    Update IniParser.py

--- IniParser.py
+++ IniParser.py
@@ -1,7 +1,10 @@
 import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
+print("SECTIONS OF CONFIG FILE:")
 print(config.sections())
+print("SET NUMBER:")
 print(config['Main']['Number'])
 DEFAULT = config['Main']
+print("SET FORWARDGO:")
 print(DEFAULT.get("ForwardGO"))
